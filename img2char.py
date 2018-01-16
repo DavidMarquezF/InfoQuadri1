3743e26ac1ac5f7785cd38ae7de81e25df02a628
irene mollet <imsala19999@gmail.com>
2018-01-16 20:02:57 +0100

    imgtoChar en funcions

--- img2char.py
+++ img2char.py
@@ -6,31 +6,41 @@ def getPatr(patrons):
         patronsImg[i] = discret.rgb_to_bn(patro)
     return patronsImg
 
-if(__name__ =="__main__"):
-    s = sys.argv[1:]
-    patrons = s[0]
-    matricula = s[1]
-
-
-    patronsImg = getPatr(patrons)
+def getMatr(matricula, hPatrons):
     matriculaImg = imgio.read_rgb(matricula)
     matriculaImg = discret.rgb_to_bn(matriculaImg)
     matriculaImg = transf.vtrim(matriculaImg)
-    hPatrons = img.get_h(patronsImg[0])
     matriculaImg = transf.scale(matriculaImg, hPatrons)
-    imgio.show(matriculaImg)
+
+    return  matriculaImg
+
+
+def getMatriculaNumbers(matriculaImg, patronsImg):
     matriculaNumbers = []
     matriculaSplit = matriculaImg
+
     while True:
         splt = split.split_digit(matriculaSplit)
-        if(splt == img.null()):
+        if (splt == img.null()):
             break
         numSplt = transf.htrim(splt[0])
         matriculaMatch = match.match(numSplt, patronsImg)
         matriculaNumbers.append(matriculaMatch)
         matriculaSplit = splt[1]
-        if(len(matriculaSplit[1]) == 0):
+        if (len(matriculaSplit[1]) == 0):
             break
-    print matriculaNumbers
+    return matriculaNumbers
+
+if(__name__ =="__main__"):
+    s = sys.argv[1:]
+    patrons = s[0]
+    matricula = s[1]
+
+    patronsImg = getPatr(patrons)
+    hPatrons = img.get_h(patronsImg[0])
+
+    matriculaImg = getMatr(matricula, hPatrons)
+
+    print getMatriculaNumbers(matriculaImg,patronsImg)
 
 
