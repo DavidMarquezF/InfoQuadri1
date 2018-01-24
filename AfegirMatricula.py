b2517359918e8832bee9aa4d4471054d9ac1de33
David <dmarquez.estudis@gmail.com>
2018-01-24 18:30:03 +0100

    Doctestos

--- AfegirMatricula.py
+++ AfegirMatricula.py
@@ -2,6 +2,9 @@ import img2char, os.path, Multa
 
 #---------------------Afegir Multa
 def askFile():
+    """
+    Demana el fitxer
+    """
     while True:
         f = raw_input("Introdueixi el fitxer amb la imatge")
         if(os.path.isfile(f)):
@@ -10,13 +13,19 @@ def askFile():
             print f + " no es un fitxer"
 
 def askFee():
+    """
+    Demana la multa
+    """
     while True:
         fee = raw_input("Introdueixi la multa (en euros)")
         if(Multa.checkIfInt(fee)):
             print "Multa de " + fee + " sera afegida a la matricula"
             return int(fee)
 
-def AfegirMulta(patr, dic):
+def afegirMulta(patr, dic):
+    """
+    Afegeix una multa a una matricula
+    """
     matr = askFile()
     matricula = img2char.nombreMatricula(patr, matr)
     multa = askFee()
@@ -27,9 +36,14 @@ def AfegirMulta(patr, dic):
     return dic
 
 def dicToString(dic):
+    """
+    Passa d'un diccionari a una llista de string
+    >>> dicToString({"4555":12, "6485":47})
+    ['4555/12', '6485/47']
+    """
     listText = []
     for key in dic.keys():
-        listText.append(key+"/"+dic[key]);
+        listText.append(key+"/"+str(dic[key]));
     return listText
 
 def writeToFile(file, listTxt):
