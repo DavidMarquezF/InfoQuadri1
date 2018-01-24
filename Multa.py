93a33eb5a66c0acd568a8fbf9317f7a765b2f6c7
David <dmarquez.estudis@gmail.com>
2018-01-24 18:34:24 +0100

    Mogut modul afegir matricula al fitxer Multa

--- Multa.py
+++ Multa.py
@@ -1,4 +1,6 @@
 from getpass import getpass
+import img2char, os.path
+
 
 Usuari = "Agent1"
 Password = "policia"
@@ -22,6 +24,9 @@ def checkIfInt(number):
 
 
 def consultarMultes(fitxer):
+    """
+    Mostra multes per pantalla
+    """
     f=open(fitxer)
     linia=f.readline()
     comptador=0
@@ -32,7 +37,9 @@ def consultarMultes(fitxer):
     f.close()
 
 def logIn():
-
+    """
+    Log in pel policia
+    """
     usuari=input("Entri usuari: ")
     password=getpass("Entri contrasenya: ")
     while usuari!=Usuari and password !=Password:
@@ -41,3 +48,61 @@ def logIn():
         password=getpass("Entri contrasenya: ")
 
     return True
+
+#---------------------Funcions generals
+def dicToString(dic):
+    """
+    Passa d'un diccionari a una llista de string
+    >>> dicToString({"4555":12, "6485":47})
+    ['4555/12', '6485/47']
+    """
+    listText = []
+    for key in dic.keys():
+        listText.append(key+"/"+str(dic[key]));
+    return listText
+
+def writeToFile(file, listTxt):
+    f = open(file,"w")
+    f.writelines(listTxt)
+    f.close()
+
+
+#---------------------Afegir Multa
+def askFile():
+    """
+    Demana el fitxer
+    """
+    while True:
+        f = raw_input("Introdueixi el fitxer amb la imatge")
+        if(os.path.isfile(f)):
+            return f
+        else:
+            print f + " no es un fitxer"
+
+def askFee():
+    """
+    Demana la multa
+    """
+    while True:
+        fee = raw_input("Introdueixi la multa (en euros)")
+        if(checkIfInt(fee)):
+            print "Multa de " + fee + " sera afegida a la matricula"
+            return int(fee)
+
+def afegirMulta(patr, dic):
+    """
+    Afegeix una multa a una matricula
+    """
+    matr = askFile()
+    matricula = img2char.nombreMatricula(patr, matr)
+    multa = askFee()
+    if(dic.has_key(matricula)):
+        dic[matricula] += multa
+    else:
+        dic[matricula] = multa
+    return dic
+
+
+
+
+
